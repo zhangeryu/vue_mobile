d7ebfe11a56be5814f787317b8cbb2a0efa84095
zhangeryu <893473916@qq.com>
2019-08-08 21:50:04 +0800

    配置项目文件

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,33 @@
-module.exports = {
-  plugins: {
-    autoprefixer: {}
+// module.exports = {
+//   plugins: {
+//     'autoprefixer': {
+//       overrideBrowserslis: ['Android >= 4.0', 'iOS >= 7']
+//     },
+//     'postcss-pxtorem': {
+//       rootValue: 37.5,
+//       propList: ['*']
+//     }
+//   }
+// }
+const { sep } = require('path')
+//  a/b/c  sep是路径中的分隔符，为了兼容不同操作系统
+module.exports = ({ file }) => {
+  // console.log(file)
+  // 当打包的时候，所有跟样式相关的文件都会经过此处
+  //    css/less/sass/vue
+  // 判断当前样式文件是否是 vant 的，如果是vant的样式文件，基准值设置为 37.5
+  // 如果是自己写的文件，基准值和设计图保持一致 75
+  let rootValue = file.dirname.indexOf(`node_modules${sep}vant`) !== -1
+    ? 37.5
+    : 75
+
+  return {
+    plugins: {
+      autoprefixer: {},
+      'postcss-pxtorem': {
+        rootValue,
+        propList: ['*']
+      }
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入vant组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 导入配置REM基准值依赖
+import 'amfe-flexible'
+// 注册到vue实例中
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
